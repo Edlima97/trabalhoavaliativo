b29e62b69baf2742d1fdad4800531fb18375224e
edvan1997 <edvinci0000@gmail.com>
2022-03-31 21:11:58 -0300

    ver-0.8

--- Ver 0.1/js/index6.js
+++ Ver 0.1/js/index6.js
@@ -1,35 +1,14 @@
 
+let ativo1 = document.querySelector('.ativo1')
+let ativo2 = document.querySelector('.ativo2')
+let ativo3 = document.querySelector('.ativo3')
+let ativo4 = document.querySelector('.ativo4')
 
 let show0= document.querySelector('.show0')
 let show1= document.querySelector('.show1')
 let show2= document.querySelector('.show2')
 let show3= document.querySelector('.show3')
 let show4= document.querySelector('.show4')
-
-let ativo1 = document.querySelector('.ativo1')
-let ativo2 = document.querySelector('.ativo2')
-let ativo3 = document.querySelector('.ativo3')
-let ativo4 = document.querySelector('.ativo4')
-
-let ativo01 = document.querySelector('.ativo01')
-let ativo02 = document.querySelector('.ativo02')
-let ativo03 = document.querySelector('.ativo03')
-let ativo04 = document.querySelector('.ativo04')
-
-let ativo001 = document.querySelector('.ativo001')
-let ativo002 = document.querySelector('.ativo002')
-let ativo003 = document.querySelector('.ativo003')
-let ativo004 = document.querySelector('.ativo004')
-
-let ativo0001 = document.querySelector('.ativo0001')
-let ativo0002 = document.querySelector('.ativo0002')
-let ativo0003 = document.querySelector('.ativo0003')
-let ativo0004 = document.querySelector('.ativo0004')
-
-let ativo00001 = document.querySelector('.ativo00001')
-let ativo00002 = document.querySelector('.ativo00002')
-let ativo00003 = document.querySelector('.ativo00003')
-let ativo00004 = document.querySelector('.ativo00004')
 
 ativo1.addEventListener('click', function(){
     show1.classList.remove('show1')
@@ -48,133 +27,14 @@ ativo2.addEventListener('click', function(){
 ativo3.addEventListener('click', function(){
     show3.classList.remove('show3')
     show0.style.display='none'
-    show1.classList.add('show1')
     show2.classList.add('show2')
     show4.classList.add('show4')
+    show1.classList.add('show1')
 })
 ativo4.addEventListener('click', function(){
     show4.classList.remove('show4')
     show0.style.display='none'
-    show1.classList.add('show1')
     show2.classList.add('show2')
     show3.classList.add('show3')
-})
-
-
-ativo01.addEventListener('click', function(){
-    show1.classList.remove('show1')
-    show0.style.display='none'
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo02.addEventListener('click', function(){
-    show2.classList.remove('show2')
-    show0.style.display='none'
     show1.classList.add('show1')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo03.addEventListener('click', function(){
-    show3.classList.remove('show3')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show4.classList.add('show4')
-})
-ativo04.addEventListener('click', function(){
-    show4.classList.remove('show4')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-})
-
-
-ativo001.addEventListener('click', function(){
-    show1.classList.remove('show1')
-    show0.style.display='none'
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo002.addEventListener('click', function(){
-    show2.classList.remove('show2')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo003.addEventListener('click', function(){
-    show3.classList.remove('show3')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show4.classList.add('show4')
-})
-ativo004.addEventListener('click', function(){
-    show4.classList.remove('show4')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-})
-
-ativo0001.addEventListener('click', function(){
-    show1.classList.remove('show1')
-    show0.style.display='none'
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo0002.addEventListener('click', function(){
-    show2.classList.remove('show2')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo0003.addEventListener('click', function(){
-    show3.classList.remove('show3')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show4.classList.add('show4')
-})
-ativo0004.addEventListener('click', function(){
-    show4.classList.remove('show4')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-})
-
-
-ativo00001.addEventListener('click', function(){
-    show1.classList.remove('show1')
-    show0.style.display='none'
-    show2.classList.add('show2')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo00002.addEventListener('click', function(){
-    show2.classList.remove('show2')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show3.classList.add('show3')
-    show4.classList.add('show4')
-})
-ativo00003.addEventListener('click', function(){
-    show3.classList.remove('show3')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show4.classList.add('show4')
-})
-ativo00004.addEventListener('click', function(){
-    show4.classList.remove('show4')
-    show0.style.display='none'
-    show1.classList.add('show1')
-    show2.classList.add('show2')
-    show3.classList.add('show3')
 })
