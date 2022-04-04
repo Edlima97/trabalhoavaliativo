8994ce6cadab15fd190699f30efc0e1779ae2a29
edlima97 <edvanlimadev@gmail.com>
2022-04-04 12:10:29 -0300

    ver-1.0

--- Ver 0.1/js/javascript.js
+++ Ver 0.1/js/javascript.js
@@ -9,4 +9,33 @@ menu.addEventListener('click', function(){
 fecha.addEventListener('click', function(){
     nav.style.display = 'none'
     console.log('fecha')
+})
+
+
+let abrir = document.querySelector('.btn-icon-1')
+let fechar = document.querySelector('.fa-times-circle')
+let noti1 = document.querySelector('.noti1')
+
+abrir.addEventListener('click', function(){
+    noti1.style.display = 'block'
+    console.log('abrir')
 }) 
+fechar.addEventListener('click', function(){
+    noti1.style.display = 'none'
+    console.log('fecha')
+});
+
+let abra = document.querySelector('.btn-icon-2')
+let noti2 = document.querySelector('.noti2')
+let exit = document.querySelector('.exit')
+
+abra.addEventListener('click', function(){
+    noti2.style.display = 'block'
+    console.log('abrasdsadair')
+}) 
+exit.addEventListener('click', function(){
+    noti2.style.display = 'none'
+    console.log('fecha')
+}) 
+
+
